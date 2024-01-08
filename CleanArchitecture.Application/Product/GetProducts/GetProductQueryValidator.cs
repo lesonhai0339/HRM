0aa0671b99a889bf8461c6de72b9c0df05d04064
kong18 <thanh2000cong20000@gmail.com>
2024-01-08 20:33:19 +0700

    CRUD PRODUCT

---
+++ CleanArchitecture.Application/Product/GetProducts/GetProductQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Product.GetProducts
+{
+    public class GetProductQueryValidator : AbstractValidator<GetProductsQuery>
+    {
+        public GetProductQueryValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty().WithMessage("Name cannot be empty.")
+                .When(query => query.Name != null);
+
+        }
+    }
+}
---
+++ CleanArchitecture.Application/Product/ProductDto.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Common.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Product
+{
+    public class ProductDto : IMapFrom<Domain.Entities.Product>
+    {
+        public ProductDto()
+        {
+
+        }
+
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Guid CustumerId { get; set; }
+        public Guid ShopId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.Product, ProductDto>();
+        }
+
+    }
+}
